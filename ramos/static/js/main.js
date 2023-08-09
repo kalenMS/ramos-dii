6a45cf49773cbabd8160e76c18793a1eb1aa65cf
kalenMS <contact.kalenms@gmail.com>
2023-08-08 21:43:20 -0400

    little upgrade

--- ramos/static/js/main.js
+++ ramos/static/js/main.js
@@ -1,26 +1,27 @@
-// function malla_check(value) {
-//     $.ajax({
-//       url: '/ramo_query/' + value + '/',
-//       method: 'GET',
-//       success: function(response) {
-//         // response.ramos123
-//         // dentro de un for, ie for ramos in ramos123:
+function malla_check(value) {
+  $.ajax({
+    url: '/ramo_query/' + value + '/',
+    method: 'GET',
+    success: function(response) {
+      console.log("Esta parte a funcionado")
+      // response.ramos123
+      // dentro de un for, ie for ramos in ramos123:
+      
+      selector = document.getElementById('ramo_select');
+                              
+      // Se crea el elemento option para añadirlo al final del ramos select
+      var option = document.createElement("option");
+      // id para crear la 2da funcion que se encarga de obtener los comentarios del ramo
+      option.value = Ramos.codigo;
+      // Nombre que se muestra en el selector de ramos
+      option.innerText = Ramos.nombre;
 
-//         selector = document.getElementById('ramo_select');
-                                
-//         // Se crea el elemento option para añadirlo al final del ramos select
-//         var option = document.createElement("option");
-//         // id para crear la 2da funcion que se encarga de obtener los comentarios del ramo
-//         option.value = ramos.id;
-//         // Nombre que se muestra en el selector de ramos
-//         option.innerText = ramos.nombre;
+      selector.appendChild(option);
+      // fin del for
 
-//         selector.appendChild(option);
-//         // fin del for
-
-//       },
-//       error: function(xhr, errmsg, err) {
-//         console.log('Error en la solicitud Ajax: ' + err);
-//       }
-//     });
-//   }
+    },
+    error: function(xhr, errmsg, err) {
+      console.log('Error en la solicitud Ajax: ' + err);
+    }
+  });
+}
--- ramos/views.py
+++ ramos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Ramos
 from django.http import JsonResponse
+
  
 # Create your views here.
 
@@ -11,5 +12,4 @@ def ramo_query(request, value_id):
     # get_object_or_404(Model, parametro = algo)
     ramos = get_object_or_404(Ramos, version=value_id)
     return JsonResponse({"ramos123" : ramos})
-
 
