8941e511a070bae188747726530a5ff3e62c82f7
kalenMS <contact.kalenms@gmail.com>
2023-09-06 09:44:35 -0300

    Listo prototipo estandar: Sin cein/ Sin opiniones

--- ramos/static/js/main.js
+++ ramos/static/js/main.js
@@ -1,21 +1,3 @@
-const darktheme = () =>{
-  document.querySelector("body").setAttribute("data-bs-theme", "dark");
-  document.querySelector("#icono").setAttribute("d", "M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z")
-  document.querySelector(".subencabezado").setAttribute("class", "subencabezado_dark");
-}
-
-const lighttheme = () =>{
-  document.querySelector("body").setAttribute("data-bs-theme", "light");
-  document.querySelector("#icono").setAttribute("d", "M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z")
-  document.querySelector(".subencabezado_dark").setAttribute("class", "subencabezado");
-}
-
-const changetheme = () =>{
-  document.querySelector("body").getAttribute("data-bs-theme") === "light"?
-  darktheme() : lighttheme();
-}
-
-
 function list_function(value) {
   var selector = document.getElementById('ramo_select');
   // Limpiar el selector antes de agregar nuevas opciones
@@ -83,7 +65,7 @@ function content_function(value) {
         // Link ucursos profesores
         selector2 = document.getElementById("profesores");
         var a = document.createElement('a');
-        a.href = "https://www.u-cursos.cl/ingenieria/2/encuestas_institucion/buscar";
+        a.href = "https://www.u-cursos.cl/ingenieria/2/encuestas_institucion/resumen_persona?id=6572f3602160e31ad10470aa8b8e82b1";
         a.target = "_blank";
         a.innerText = "U-cursos";
         selector2.appendChild(a);
@@ -132,4 +114,23 @@ function content_function(value) {
       }
     });
   }
+}
+
+
+
+const darktheme = () =>{
+  document.querySelector("body").setAttribute("data-bs-theme", "dark");
+  document.querySelector("#icono").setAttribute("d", "M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z")
+  document.querySelector(".subencabezado").setAttribute("class", "subencabezado_dark");
+}
+
+const lighttheme = () =>{
+  document.querySelector("body").setAttribute("data-bs-theme", "light");
+  document.querySelector("#icono").setAttribute("d", "M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z")
+  document.querySelector(".subencabezado_dark").setAttribute("class", "subencabezado");
+}
+
+const changetheme = () =>{
+  document.querySelector("body").getAttribute("data-bs-theme") === "light"?
+  darktheme() : lighttheme();
 }
